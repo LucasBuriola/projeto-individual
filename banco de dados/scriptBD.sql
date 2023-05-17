e85cefcf2368985d63d553ba1c595d02449bb602
GabrielBoos22 <gabriel.bduarte@sptech.school>
2023-05-17 15:19:48 -0300

    api

---
+++ banco de dados/scriptBD.sql
@@ -0,0 +1,13 @@
+create database strengthofmotivation;
+use strengthofmotivation;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY auto_increment,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+
+
+select * from usuario; 	
