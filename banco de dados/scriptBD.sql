d4b9d8dabdb9a8e8dc05e8beae63e1a95e5e9808
LucasBuriola <lucas.buriola@sptech.school>
2023-06-01 15:12:05 -0300

    site

--- banco de dados/scriptBD.sql
+++ banco de dados/scriptBD.sql
@@ -9,5 +9,34 @@ CREATE TABLE usuario (
 );
 
 
+CREATE TABLE imc (
+	idImc int PRIMARY KEY auto_increment,
+	PesoKG VARCHAR(50),
+	AlturaCM VARCHAR(200),
+);
+
+CREATE TABLE Gasto_de_calorias (
+	idGC int PRIMARY KEY auto_increment,
+	genero VARCHAR(45),
+	PesoKG VARCHAR(50)
+
+);
+
+CREATE TABLE Treino (
+	idT int PRIMARY KEY auto_increment,
+	Treino VARCHAR(45)
+); 
+
+CREATE TABLE musculo (
+	idM int PRIMARY KEY auto_increment,
+	peito VARCHAR(45),
+	biceps VARCHAR(45),
+	triceps VARCHAR(45),
+	costas VARCHAR(45),
+	antebraço VARCHAR(45),
+	perna VARCHAR(45),
+	ombro VARCHAR(45)
+);
+
 
 select * from usuario; 	
